44d2154c171661ad9ca5e162ffede24fba4f138c
John Gibson <jkgibson@ieee.org>
2016-12-01 21:28:11 -0800

    Add: Added ability to print HTML to screen.

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -12,7 +12,7 @@ var quotes = [
 		quote: 		text of quote to de displayed on screen. string
 		source: 	who wrote it. string
 		citation: 	where it comes from. i.e. Webster's, etc. optional. string
-		year: 		year it was created. optional. string 
+		year: 		year it was created. optional. string
 	}
 ]
 */
@@ -32,14 +32,14 @@ var quotes = [
 	{
 		quote: 		"John is awesomest",
 		source: 	"Amy Gibson",
-		citation: 	"Amy's Book of Awesomeness",
-		year: 		"2016"
+		citation: 	"",
+		year: "2016"
 	},
 	{
 		quote: 		"John is awesomest bro",
 		source: 	"Stephanie Gibson",
 		citation: 	"Stephanie's Book of Awesomeness",
-		year: 		"2016"
+		year:       ""
 	}
 
 ]
@@ -53,19 +53,18 @@ Return: random quote from the array
 
 //Grabbed this function from MDN
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  var min = Math.ceil(min);
+  var max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
 function getRandomQuote() {
-	ran_quote_pos = getRandomIntInclusive(0, quotes.length);
+	var ran_quote_pos = getRandomIntInclusive(0, quotes.length);
 	return quotes[ran_quote_pos];
-
 }
 
-console.log(getRandomQuote)
+//console.log(getRandomQuote)
 /*
 TODO: printQuote: prints the quote by
 	1) call the getRandomQuote function and store returned quote object in a variable
@@ -81,24 +80,36 @@ TODO: printQuote: prints the quote by
 	3) printQuote doesn't print if there is a missing citation or if year property is mssing
 	4) will display the final HTML to the page. Use the following JS snippet to accomplish it:
 		document.getElementById('quote-box').innerHTML
-	5) When quote changes, randomly change background color of page.
-	6) Don't display a random quote more than once until ALL quotes from the array have been displayed. 
-	   To help reviewers (and yourself) verify that the quotes don’t repeat until they’ve all been displayed, 
+	5) TODO: When quote changes, randomly change background color of page.
+	6) TODO: Don't display a random quote more than once until ALL quotes from the array have been displayed.
+	   To help reviewers (and yourself) verify that the quotes don’t repeat until they’ve all been displayed,
 	   log the quote to the console each time the “Show Another Quote” button is clicked.
-	7) Refresh the quote after say..30 sec.
+	7) TODO: Refresh the quote after say..30 sec.
 */
+
+function print(message) {
+  var outputDiv = document.getElementById('quote-box');
+  outputDiv.innerHTML = message;
+}
+
 function printQuote() {
-	var quote = getRandomQuote();
-	var quote_body = "<p class='quote'>" + quote.quote + "</p>";
-	var source = "<p class = 'source'>" + quote.source;
-	var citation = "<span class='citation'>" + quote.citation + "</span>";
-	var year = "<span class='year'>" + quote.year + "</span>";
-	var HTML_temp = quote_body.concat(source, citation,year,"</p>");
-	return HTML_temp;
+	var text = getRandomQuote();
+	if (text.citation === "" || text.year === "") {
+		print("<p class='quote'>" + "You didn't include a year or citation!</p>");
+	} else {
+		var text_body = "<p class='quote'>" + text.quote + "</p>";
+		var source = "<p class='source'>" + text.source;
+		var citation = "<span class='citation'>" + text.citation + "</span>";
+		var year = "<span class='year'>" + text.year + "</span>";
+		var HTML_temp = text_body.concat(source, citation,year,"</p>");
+		print(HTML_temp);
+	}
 }
 
 
 
+
+//document.getElementById('quote-box').innerHTML;
 
 
 
@@ -110,5 +121,5 @@ function printQuote() {
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
