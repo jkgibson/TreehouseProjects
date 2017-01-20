de7e38eca68c10fbdb6353b9121aef8c8b30455e
John Gibson <jkgibson@ieee.org>
2017-01-19 16:27:44 -0800

    CHANGE: Debugged the display of pages. Added more comments

--- Project2/js/main.js
+++ Project2/js/main.js
@@ -5,14 +5,13 @@
 
 //When page loads, fire off our script
 
-	var endOfPage = document.getElementsByClassName('page')[0];
+	var endOfPage = document.getElementsByClassName("page")[0];
 	var students = document.getElementsByClassName("student-item");
 	var numStudents = students.length;
 
 
 		//Calculate the number of pages needed
 	function numberOfPagesNeeded(numStudents) {
-
 		//Divide the count result by 10 to get the number of pages
 		var numPages = Math.ceil(numStudents/10);
 		return numPages;
@@ -33,9 +32,6 @@
   		}
   	}
 
-
-
-
 	//Create a pagination function to do the pagination
 	function paginator() {
 		var pagination_div = document.createElement("div");
@@ -45,34 +41,27 @@
  		//Iterate through the number of pages that we have
 		//Set the page number text to current loop value
 		for(var i = 0; i < numberOfPagesNeeded(numStudents); i++) {
-			//Create a list element
+			//Create a list element and anchor element
 			var list = document.createElement("li");
 			var anchor = document.createElement("a");
 			//Set attribute values for the anchor tag
 			anchor.setAttribute("href", "#");
 			//Set the page number text to current loop value
-			anchor.innerHTML = i;
+			anchor.innerHTML = i + 1;
 			//Append the anchor to the li element
 			list.appendChild(anchor);
 			//Append the list element to the unordered list
 			ul.appendChild(list);
 		}
-		//Now display the .pagination HTML, which is the ul we created up above
+		//Appply the pagination by successively appending to the DOM
 		pagination_div.appendChild(ul);
 		endOfPage.appendChild(pagination_div);
 	};
 
 
-	paginator();
-
-	//need to functionalize this step right here
-	var allAnchorsInDiv = document.querySelectorAll('a');
-
-	for(var i = 0; i < allAnchorsInDiv.length; i++) {
-    	allAnchorsInDiv[i].addEventListener("click", onActivePage);
-	};
-
-
+	//When the user clicks on a pagination button, we need to hide all of the students,
+	//check for any active elements, remove them, add the current clicked element as active,
+	//and show 10 students on a page at a time.
 	function onActivePage() {
 		hideStudents(numStudents,students);
 		//if there are any active elements, clear them and add active to the currect element
@@ -82,10 +71,9 @@
 			};
 		};
 
-		this.classList.add('active');
+		this.classList.add("active");
 
 		//Now, we need to show 10 students, based on the current active page we are on
-
 		var currentPage = parseInt(this.innerText);
 		console.log(currentPage);
 		var endPage = currentPage * 10;
@@ -93,10 +81,31 @@
 		for(var i = startPage; i < endPage; i++) {
 			students[i].style.display = "block";
 		}
-
-
-		console.log(allAnchorsInDiv);
 	};
+
+	//This function will add a event listener for click on the pagination buttons
+	function setClickEvent(allAnchorsInDiv) {
+		for(var i = 0; i < allAnchorsInDiv.length; i++) {
+	    	allAnchorsInDiv[i].addEventListener("click", onActivePage);
+		};
+	};
+
+
+	//Initially show ten students when we launch the page
+	showTenStudents(students);
+
+	//Do the pagination changes to the HTML
+	paginator();
+
+	
+	//Select all of the anchor elements in our pagination
+	var allAnchorsInDiv = document.querySelectorAll("a");
+
+	//Set the click event on all of the anchors so you can be ready for a click.
+	setClickEvent(allAnchorsInDiv);
+
+	
+	
 
 
 
